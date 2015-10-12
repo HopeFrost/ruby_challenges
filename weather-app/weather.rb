cd907a823c49b4c32dd7e4a7bfb95abf5c71d6b1
HopeFrost <hopefrost14@gmail.com>
2015-10-12 00:59:12 -0400

    weather update

--- weather-app/weather.rb
+++ weather-app/weather.rb
@@ -2,15 +2,31 @@
 
 require 'yahoo_weatherman'
 
-puts "what is your area code?"
-
-location=gets.chomp
-
-client = Weatherman::Client.new
-
-client.lookup_by_location(location).condition['temp']
 
 
- weather.forecasts.each do |forecast|
-   puts "#{forecast['day']} is going to be #{forecast['text'].downcase} with a low of #{forecast['low']} and a high of #{forecast['high']}"
+  client = Weatherman::Client.new
+  puts "what is your area code?"
+  location = gets.chomp
+  condition = client.lookup_by_location(location).condition['text']
+  puts "Its curently #{condition}."
+
+
+forecast = client.lookup_by_location(location).forecasts
+today= Time.now.strftime('%w').to_i
+day = forecast['date']
+weekday = day.strftime('%w').to_i
+
+weekdays.each do |forcast|
+
+if weekday == today
+   dayName = 'Today'
+elsif weekday == today + 1
+   dayName = 'Tomorrow'
+else
+   dayName = day.strftime('%A')
+end
+forecast = client.lookup_by_location(location).forecasts
+   puts "#{dayName} is going to be #{forecast['text'].downcase} with a low of #{forecast['low']} and a high of #{forecast['high']}"
  end
+
+ weather_condition
