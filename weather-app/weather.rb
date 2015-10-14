74783db6b2a50d6a4e900f06fbac166eaf3952de
HopeFrost <hopefrost14@gmail.com>
2015-10-14 11:19:26 -0400

    weather, add spec

--- weather-app/weather.rb
+++ weather-app/weather.rb
@@ -1,24 +1,20 @@
 
-
 require 'yahoo_weatherman'
-
-
 
   client = Weatherman::Client.new
   puts "what is your area code?"
   location = gets.chomp
-  condition = client.lookup_by_location(location).condition['text']
+  respond = client.lookup_by_location(location)
+  condition = respond.condition['text']
   puts "Its curently #{condition}."
 
-date = client.lookup_by_location(location).condition['date']
+date = Time.now
 today= date.strftime('%w').to_i
 
-day = client.lookup_by_location(location).forecasts['date']
-print day
-weekday = day.strftime('%w').to_i
 
-
-weekdays.each do |forcast|
+respond.forecasts.each do |forecast|
+  day= forecasts['date']
+  weekday = day.strftime('%w').to_i
 
 if weekday == today
    dayName = 'Today'
@@ -27,7 +23,6 @@ elsif weekday == today + 1
 else
    dayName = day.strftime('%A')
 end
-forecast = client.lookup_by_location(location).forecasts['low', 'hight']
-forecastCondition = client.lookup_by_location(location).forecasts['text']
-   puts "#{dayName} is going to be #{forecastCondition['text'].downcase} with a low of #{forecast['low']} and a high of #{forecast['high']}"
+forecast_condition = forecasts['text']
+   puts "#{dayName} is going to be #{forecast_condition.downcase} with a low of #{forecast['low']} and a high of #{forecast['high']}"
 end
